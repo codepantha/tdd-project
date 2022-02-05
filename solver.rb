8c8c3a3826e5bfe2772c255bec3c0ab524c3ca91
Amrhub <amr_ahmed65@hotmail.com>
2022-02-05 13:06:42 +0200

    add reverse method

--- solver.rb
+++ solver.rb
@@ -1,9 +1,20 @@
 class Solver
   def factorial(num)
     fact = 1
-    (1..num).each do |i|
-      fact *= i
-    end
+    (1..num).each { |i| fact *= i }
     fact
+  end
+
+  def reverse(str)
+    new_str = ''
+    i = str.length - 1
+
+    loop do
+      new_str << str[i]
+      break if i.zero?
+
+      i -= 1
+    end
+    new_str
   end
 end
