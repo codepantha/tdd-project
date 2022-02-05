ce4efc19cbf94a1fcb144f7066b04f57809a5706
codepanther <pezechigozirim@gmail.com>
2022-02-05 12:17:03 +0100

    Add test for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -14,5 +14,21 @@ describe Solver do
     it 'reverse should return "olleh" when passed "hello"' do
       expect(@solver.reverse('hello')).to eq 'olleh'
     end
+
+    it "fizzbuzz should return 'fizz' when passed in 3" do
+      expect(@solver.fizzbuzz(3)).to eq 'fizz'
+    end
+  
+    it "fizzbuzz should return 'buzz' when passed in 5" do
+      expect(@solver.fizzbuzz(5)).to eq 'buzz'
+    end
+
+    it "fizzbuzz should return 'fizzbuzz' when passed in 15" do
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+
+    it "fizzbuzz should return '7' when passed in 7" do
+      expect(@solver.fizzbuzz(7)).to eq '7'
+    end
   end
 end
