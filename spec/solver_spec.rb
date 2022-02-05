2654515dc999bcd38548d342b49234c6adaa978b
Amrhub <amr_ahmed65@hotmail.com>
2022-02-05 13:06:36 +0200

    test reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,9 +2,17 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
+  before(:each) do
+    @solver = Solver.new
+  end
+
   context 'When testing Solver class' do
-    it 'factorial should return 125 when passed 5' do
-      expect(Solver.new.factorial(5)).to eq 120
+    it 'factorial should return 120 when passed 5' do
+      expect(@solver.factorial(5)).to eq 120
+    end
+
+    it 'reverse should return "olleh" when passed "hello"' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
     end
   end
 end
